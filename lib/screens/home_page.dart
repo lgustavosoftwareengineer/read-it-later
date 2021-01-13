26f08ad4b3edd80c61ed46166920288866240dd2
Luiz Gustavo <gugamatias7@gmail.com>
2021-01-13 11:07:37 -0300

    Listing all the books added in the booksSaved list

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:read_it_later/controllers/books_saved.controller.dart';
+import 'package:read_it_later/models/Book.dart';
 import 'package:read_it_later/widgets/app_bar.item.dart';
+import 'package:read_it_later/widgets/card.item.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,27 +10,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
+  List<Book> books;
   @override
   void initState() {
-
+    setState(() {
+      books = booksSavedController.showAll();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(text: 'Qual será a próxima leitura?'),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: Colors.white,
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Página inicial')
-          ],
-        ),
-      ),
-    );
+        appBar: CustomAppBar(text: 'Qual será a próxima leitura?'),
+        body: AnimatedBuilder(
+          animation: booksSavedController,
+          builder: (context, child) {
+            if (books.length < 1) {
+              return Center(
+                child: Text('Você ainda não tem nenhum livro marcado para ler depois'),
+              );
+            }
+            return Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                color: Colors.white,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: books.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return NRCard(
+                        bookTitle: books[index].title,
+                        bookAuthor: books[index].authors,
+                        imageLink: books[index].image,
+                        selfLink: books[index].selfLink,
+                        icon: Icon(Icons.check),
+                        action: () {
+                          booksSavedController.removeOneItem(index);
+                          setState(() {
+                            books = booksSavedController.showAll();
+                          });
+                        });
+                  },
+                ));
+          },
+        ));
   }
 }
