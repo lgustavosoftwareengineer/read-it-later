7c5f1d9aa43aebe9e868cdfe530d497505230abb
Luiz Gustavo <gugamatias7@gmail.com>
2021-01-13 11:06:22 -0300

    Modularizing the appBar and body widgets

--- lib/screens/create_booked.page.dart
+++ lib/screens/create_booked.page.dart
@@ -14,13 +14,6 @@ class _CreatedBookedPageState extends State<CreatedBookedPage> {
   Future<Books> books;
   bool isSearching = false;
 
-  // handleInput(String value) {
-  //   setState(() {
-  //     isSearching = true;
-  //     searchValue = value;
-  //   });
-  // }
-
   _onChangeHandler(value) {
     Timer searchOnStoppedTyping;
 
@@ -47,61 +40,58 @@ class _CreatedBookedPageState extends State<CreatedBookedPage> {
   }
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.search, color: Colors.blue),
-        title: TextField(
-          decoration: InputDecoration(
-              hintText: 'Digite aqui o nome do livro...',
-              hintStyle: TextStyle(color: Colors.grey),
-              border: InputBorder.none,
-              ),
-          onChanged: _onChangeHandler,
-
+    final _appBar = AppBar(
+      leading: Icon(Icons.search, color: Colors.blue),
+      title: TextField(
+        decoration: InputDecoration(
+          hintText: 'Digite aqui o nome do livro...',
+          hintStyle: TextStyle(color: Colors.grey),
+          border: InputBorder.none,
         ),
-
-        backgroundColor: Colors.white,
-        //elevation: 0,
+        onChanged: _onChangeHandler,
       ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: Colors.white,
-        child: FutureBuilder<Books>(
-          future: books,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount: snapshot.data.items.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return NRCard(
-                    bookTitle: snapshot.data.items[index].title,
-                    bookAuthor: snapshot.data.items[index].authors,
-                    imageLink: snapshot.data.items[index].image,
-                  );
-                },
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                  child: Text('Tente buscar um livro na barra logo em cima'));
-            } else if (isSearching == false) {
-              return Center(
-                  child: Text('Tente buscar um livro na barra logo em cima'));
-            }
 
-            // By default, show a loading spinner.
-            return Center(child: CircularProgressIndicator());
-          },
-        ),
+      backgroundColor: Colors.white,
+      //elevation: 0,
+    );
+
+    final _body = Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: Colors.white,
+      child: FutureBuilder<Books>(
+        future: books,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: snapshot.data.items.length,
+              itemBuilder: (BuildContext context, int index) {
+                return NRCard(
+                  bookTitle: snapshot.data.items[index].title,
+                  bookAuthor: snapshot.data.items[index].authors,
+                  imageLink: snapshot.data.items[index].image,
+                  selfLink: snapshot.data.items[index].selfLink,
+                  icon: Icon(Icons.add),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+                child: Text('Tente buscar um livro na barra logo em cima'));
+          } else if (isSearching == false) {
+            return Center(
+                child: Text('Tente buscar um livro na barra logo em cima'));
+          }
+
+          // By default, show a loading spinner.
+          return Center(child: CircularProgressIndicator());
+        },
       ),
     );
+
+    return Scaffold(appBar: _appBar, body: _body);
   }
 }
