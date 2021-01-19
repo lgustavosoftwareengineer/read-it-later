7264479e590ef5961c5d3194b3e8c6af867d30a6
Luiz Gustavo <gugamatias7@gmail.com>
2021-01-18 23:09:28 -0300

    refactor: changin the Strings  (emptyTrashPage/emptySearchPage)

--- lib/Strings.dart
+++ lib/Strings.dart
@@ -1,9 +1,9 @@
 class Strings {
   static final emptyHomePage =
       "Você ainda não tem nenhum livro marcado para ler depois";
-  static final emptyTrashPage = "Sua lixeira está vazia...";
+  static final emptyTrashPage = "Você ainda não tem nenhum livro concluído...";
   static final emptySearchBookPage =
-      "Tente buscar um livro na barra logo em cima ";
+      "Tente buscar um livro na barra de pesquisa logo em cima";
   static final error =
       "Erro ao carregar o conteúdo verifique a sua internet ou reinicie o aplicatiovo";
 }
