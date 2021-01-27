b7daf48432633affbddb10fd2fd9189b8e814c45
Luiz Gustavo <gugamatias7@gmail.com>
2021-01-27 13:06:31 -0300

    feat: change the color of the snackbar

--- lib/handlers/snackbar.handler.dart
+++ lib/handlers/snackbar.handler.dart
@@ -4,8 +4,9 @@ class SnackBarHandler {
 
   showSnackbar({BuildContext context, String message}) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        backgroundColor: Theme.of(context).accentColor,
         content:
-            Text(message),
+            Text(message, style: TextStyle(color: Colors.white)),
         duration: Duration(seconds: 2
         ),
         ));
